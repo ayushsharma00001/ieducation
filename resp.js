5cf6121d96f1770dd2965674d77f0835566abb50
ayushsharma00001 <116947667+ayushsharma00001@users.noreply.github.com>
2023-07-03 01:39:50 +0530

    Add files via upload

---
+++ resp.js
@@ -0,0 +1,13 @@
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+navlist = document.querySelector('.nav-list')
+rightnav = document.querySelector('.rightnav')
+
+
+
+
+burger.addEventListener('click',()=>{
+    navlist.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+    rightnav.classList.toggle('v-class-resp');
+})
